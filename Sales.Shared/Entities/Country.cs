bbd8fb17be4e73187da07f8c70559a3b9a0a8e95
nadresdev <nadresdev@gmail.com>
2023-11-09 12:33:52 -0500

    INITIAL COMMIT CON DB

---
+++ Sales.Shared/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El Nombre de Pais es obligatorio")]
+        [MaxLength(20)]
+        [Display(Name = "País")]
+        public string Name { get; set; } = null!;
+    }
+}
